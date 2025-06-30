4f3e25235190e82fef08fdcca0df4e0cfc084f30
Hamtone <omondiondiek@zetech.ac.ke>
2025-06-30 15:42:45 +0300

    fix: add index file for layout components

---
+++ src/components/layout/index.js
@@ -0,0 +1,4 @@
+export { default as AppLayout } from './AppLayout';
+export { default as ResponsiveContainer } from './ResponsiveContainer';
+export { default as SemaNamiLayout } from './SemaNamiLayout';
+export { default as Sidebar } from './Sidebar'; 
